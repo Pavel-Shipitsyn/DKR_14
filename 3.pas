9ae25e48dde60e50c2995d65b8386e50c5823577
Pavel-Shipitsyn <117993255+Pavel-Shipitsyn@users.noreply.github.com>
2022-12-18 12:19:12 +0100

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,50 @@
+﻿var
+  intext, out1text,out2text: file of integer;
+  a:integer;
+begin
+  Assign(intext, 'D:\LR_14\in.txt');
+  Assign(out1text, 'D:\LR_14\out.txt');
+  Assign(out2text, 'D:\LR_14\out2.txt');
+  Rewrite(intext);
+  for var i:=1 to 20 do
+  begin
+    a:=random(1,100);
+    write(intext,a);
+  end;
+  
+  
+  rewrite(out1text);
+  rewrite(out2text);
+  reset(intext);
+  while not eof(intext) do
+  begin
+    read(intext,a);
+    if a mod 2 = 1 then
+      write(out1text,a)
+    else
+      write(out2text,a);
+  end;
+  
+  
+  reset(out1text);
+  writeln('Нечетные');
+  while not eof(out1text) do
+  begin
+    read(out1text,a);
+    write(a,' ');
+  end;
+  
+  
+  reset(out2text);
+  writeln;
+  writeln('Четные');
+  while not eof(out2text) do
+  begin
+    read(out2text,a);
+    write(a,' ');
+  end;
+  
+  close(intext);
+  close(out1text);
+  close(out2text);
+end.
---
+++ 5.pas
@@ -0,0 +1,21 @@
+﻿var
+ S:String;
+ F_in,F_temp: File of Real;
+ temp:Real;
+ mas: array[1..15] of Byte;  // Переменная массива.
+i, max: Byte;
+begin
+    Assign(F_in,'C:\Users\pavel\Desktop\ОАиП\Лаб 14\in.txt.');
+  Reset(F_in);
+  Assign(F_temp,'C:\Users\pavel\Desktop\ОАиП\Лаб 14\temp.txt.');
+  ReWrite(F_temp);
+for i:=2 to 14 do
+begin
+ if mas[i]>(mas[i-1] and (mas[i]+1)) then  // Начинаем поиск локального максимума
+  if max<mas[i] then max:=mas[i];
+end;
+  Close(F_in);
+  Close(F_temp);
+  Erase(F_in); {удаление файла}
+  Rename(F_temp,'C:\Users\pavel\Desktop\ОАиП\Лаб 14\in.txt.'); {Переименование файла}
+end.
